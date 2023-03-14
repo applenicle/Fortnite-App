4451d9c1bec2bf06357a44065195df38c4fd7682
Yura <Applenicle>
2023-03-14 15:18:31 +0500

    Exports

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as Layout } from './Layout';
+export { default as Sort } from './Sort';
+export { default as ShopList } from './ShopList';
+// export { default as Details } from './Details';
+export { default as Timer } from './Timer';
