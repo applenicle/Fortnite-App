bfd170d14f8cdc1cbdade2847b25fe7028f88219
Yura <Applenicle>
2023-03-14 15:18:09 +0500

    Details

---
+++ components/Details/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Details = ({ arr }: any) => {
+  const { displayAssets } = arr || {};
+  console.log(arr);
+
+  return <div></div>;
+};
+
+export default Details;
