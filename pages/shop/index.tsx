79f06efc66f5bb8631bd07f6723db59f5d203dc6
applenicle <gamer31122002@gmail.com>
2023-03-19 01:14:06 +0500

    Shop with Redux

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,54 +1,42 @@
 import React from 'react';
-// import { useData } from '@/hooks/useData';
-import { Layout, ShopList, Sort, Timer } from '../../components';
-import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { setItems } from '@/redux/slices/Shop';
+import { Layout, ShopList, Skeleton, Timer } from '../../components';
+import { nameAPI as shopAPI } from '@/redux/services/ShopService';
 
 const Shop = () => {
-  const dispatch = useDispatch();
-  const { items } = useSelector((state: any) => state.ShopSlice);
-  const data = items.shop;
-  const fetchData = async (url: string) => {
-    await axios
-      .get(url, {
-        headers: { Authorization: process.env.NEXT_PUBLIC_FORTNITE_API_KEY },
-      })
-      .then(({ data }: any) => {
-        // console.log(data);
-        dispatch(setItems(data));
-      });
-  };
-  React.useEffect(() => {
-    fetchData('https://fortniteapi.io/v2/shop?lang=en');
-  }, []);
+  const { data: shop, error, isLoading } = shopAPI.useFetchAllShopQuery('shop');
 
+  const dataShop = shop?.shop;
   let obj: any = {};
   let arr: any = [];
-  const sortedID = [...new Set(data?.map((obj: any) => obj?.section?.id))].map(
+  const sortedID = [...new Set(dataShop?.map((obj: any) => obj?.section?.id))].map(
     (item) => (obj[item] = []),
   );
-  data?.map((item: any) => {
+  dataShop?.map((item: any) => {
     obj[item?.section?.id]?.push({ ...item });
   });
 
   Object.entries(obj).map(([id, data]) => {
     arr.push({ id, data });
   });
+  const skeleton = [...new Array(15)].map((_: any, i: number) => <Skeleton key={i} />);
+
   return (
     <>
       <Layout>
         <Timer />
-        <Sort />
-        {arr.map((obj: any) => (
-          <div key={obj.id}>
-            <h1 className="shop__title">{obj?.data[0]?.section?.name}</h1>
-            <ul className="shop__list">
-              <ShopList {...obj} />
-            </ul>
-          </div>
-        ))}
+        <>{error && <>ERROR TRY TO REFRESH PAGE</>}</>
+        {isLoading && true ? (
+          <div className="shop__list">{skeleton}</div>
+        ) : (
+          arr.map((obj: any) => (
+            <div key={obj.id}>
+              <h1 className="shop__title">{obj?.data[0]?.section?.name}</h1>
+              <ul className="shop__list">
+                <ShopList {...obj} />
+              </ul>
+            </div>
+          ))
+        )}
       </Layout>
     </>
   );
