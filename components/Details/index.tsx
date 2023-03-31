2f8cfd2caf1345a4e10824d9018b74e057479a30
applenicle <gamer31122002@gmail.com>
2023-03-31 17:59:41 +0500

    Details page

--- components/Details/index.tsx
+++ components/Details/index.tsx
@@ -1,25 +1,73 @@
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-import React from 'react';
-import Moment from 'react-moment';
 import moment from 'moment';
-import 'moment-timezone';
+
+type Introduction = {
+  chapter?: string
+  season?: string
+}
+
+type Image = {
+  background: string
+  featured: string
+  full_background: string
+  icon: string
+  icon_background: string
+}
+
+type TypeObj = {
+  id: string
+  name: string
+}
+interface ISet {
+  partOf: string
+  extended: TypeObj
+}
+interface IGrants {
+  description: string
+  id:string
+  images: Image
+  rarity: TypeObj
+  series: string | null
+  type: TypeObj
+}
+type Added = {
+  date: string
+  version: string
+}
+
+type Details = {
+  added ?: Added
+  description : string
+  grants ?: IGrants[]
+  images : Image
+  introduction : Introduction | null
+  lastAppearance ?: string
+  name : string
+  rarity : TypeObj
+  releaseDate : string
+  set : ISet | null
+  series ?: TypeObj
+  shopHistory : string[]
+  type : TypeObj 
+}
 
 const Details = ({
-  introduction,
-  series,
-  type,
-  description,
-  rarity,
-  name,
-  images,
-  shopHistory,
-  lastAppearance,
   added,
+  description,
   grants,
+  images,
+  introduction,
+  lastAppearance,
+  name,
+  rarity,
+  releaseDate,
   set,
-}: any) => {
+  series,
+  shopHistory,
+  type,
+}: Details) => {
   return (
     <div className="details">
       <div className="details__img">
@@ -31,39 +79,49 @@ const Details = ({
           <FontAwesomeIcon icon={faArrowRight} />
         </Link>
         <h3 className="details__title">{name}</h3>
+        <h2 className="details__subtitle">
+          {type.name}
+          <span className={
+              `${rarity?.name.toLowerCase().includes('uncommon') ? 'rarity-uncommon' : 'rarity'}
+                ${rarity?.name.toLowerCase().includes('rare') ? 'rarity-rare' : 'rarity'}
+                ${rarity?.name.toLowerCase().includes('epic') ? 'rarity-epic' : 'rarity'}
+                ${rarity?.name.toLowerCase().includes('legendary') ? 'rarity-legendary' : 'rarity'}`
+              }>{rarity.name}
+          </span>
+        </h2>
         <p className="details__text">{description}</p>
 
         <ul className="details__list">
           <li className="details__item">
-            <span>rare: </span>
-            {rarity.name}
+            {undefined ? `Introduced in ${introduction?.chapter} , ${introduction?.season}` : ''}
           </li>
-
-          <li className="details__item">
-            <span> Introduced in: </span>
-            {introduction?.chapter}, {introduction?.season}
-          </li>
-
+          <li className="details__item">{set?.partOf}</li>
           <li className="details__item">{series?.name}</li>
-          <li className="details__item"></li>
           <li className="details__item">
-            {series?.name}
-            {type?.name}
+            <span>Release date: </span>
+            {moment(releaseDate).format('ll')}
           </li>
           <li className="details__item">
             <span>Last seen: </span>
-            {moment(added.date[0], 'DD').fromNow()}
+            {moment(lastAppearance).format('ll')}
           </li>
-          <li className="details__item">описание тест{grants[0]?.description}</li>
+          <li className="details__item">
+          {grants?.[0]?.description}
+          </li>
           <li className="details__item">
             <span>Added: </span>
-            {added.date}
+            {moment(added?.date).format('ll')}
           </li>
-          {set?.partOf}
-          {/* <li className="details__item">
-            <span>Added: </span>
-            {shopHistory}
-          </li> */}
+          <li className="details__item">
+            History:
+            <select>
+              {shopHistory?.map((obj:string, i:number) => 
+                <option key={i}>
+                  {moment(obj).format('ll')}
+                </option>
+              ).reverse()}
+            </select>
+          </li>
         </ul>
       </div>
     </div>
