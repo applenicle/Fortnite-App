661df5f3901f2ff8a2c5e12ae6bf4e55e42b7f51
applenicle <gamer31122002@gmail.com>
2023-04-04 18:52:05 +0500

    Fish

--- pages/fish/index.tsx
+++ pages/fish/index.tsx
@@ -1,44 +1,70 @@
+import { Modal } from '@/components';
 import { Layout } from '@/layouts';
-import axios from 'axios';
+import { useGetFishQuery } from '@/redux/services/FortniteApi';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Fish: NextPage = () => {
-  const [data, setData] = React.useState([]);
-  const [open, setOpen] = React.useState(true);
+  const [activeItem, setActiveItem] = React.useState<number | null>(null);
 
-  const fetchData = async (url: string) => {
-    await axios
-      .get(url, {
-        headers: { Authorization: process.env.NEXT_PUBLIC_FORTNITE_API_KEY },
-      })
-      .then(({ data }: any) => {
-        console.log(data.fish);
-        setData(data.fish);
-      });
+  const { locale } = useRouter();
+  const { data, error, isLoading } = useGetFishQuery(locale);
+  console.log(data)
+  const onSelectItem = (index:number | null) => {
+    setActiveItem(index);
   };
 
-  React.useEffect(() => {
-    fetchData('https://fortniteapi.io/v1/loot/fish?lang=en');
-    return () => {};
-  }, []);
+  if(isLoading) {
+    <></>
+  }
+
   return (
     <Layout>
+      <h3 className="fish__title">All fish</h3>
       <ul className="fish">
-        {data.map((obj: any) => (
-          <div>
-            <div onClick={() => setOpen(!open)} className="fish__inner" key={obj.id}>
+        {data?.fish.map((obj: any, i:number) => (
+          <li key={obj.id}>
+            <div onClick={() => onSelectItem(i)}>
               <img className="fish__img" src={obj.image} alt={obj.id} />
-              <h3>{obj.name}</h3>
             </div>
-            <div className={open === false ? 'modal modal__open' : 'modal'}>
-              {/* <h3>{obj.name}</h3> */}
-              {/* <p>{obj.details}</p> */}
-              {/* <span>Rarity: {obj.rarity}</span> */}
-              {/* <div>Макс колво{obj.maxStackSize}</div> */}
-              {/* <p>Description: {obj.description}</p> */}
+            <div className={activeItem === i ? 'modal modal__open' : 'modal'}>
+              <div className="fish__inner">
+                <div className="fish__close">
+                  <FontAwesomeIcon 
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      onSelectItem(null)
+                    }} 
+                    icon={faXmark} 
+                  />
+                </div>
+                <img src={obj.image} alt={obj.id} />
+                <div className="fish__content">
+                  <h3>{obj.name}</h3>
+                  <p>{obj.details}</p>
+                  <ul>
+                    <li className={
+                      `
+                        ${obj.rarity.toLowerCase().includes('common') ? 'rarity-common' : 'rarity'}
+                        ${obj.rarity.toLowerCase().includes('uncommon') ? 'rarity-uncommon' : 'rarity'}
+                        ${obj.rarity.toLowerCase().includes('rare') ? 'rarity-rare' : 'rarity'}
+                        ${obj.rarity.toLowerCase().includes('epic') ? 'rarity-epic' : 'rarity'}
+                        ${obj.rarity.toLowerCase().includes('legendary') ? 'rarity-legendary' : 'rarity'}`
+                    }>
+                      {obj.rarity}
+                    </li>
+                    <li>{obj.description}</li>
+                    <li>Maximum stack {obj.maxStackSize}</li>
+                    <li>Heal {obj.heal}</li>
+                    <li><span>{obj.sizeMin}</span> cm - <span>{obj.sizeMax}</span> cm</li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
+          </li>
         ))}
       </ul>
     </Layout>
