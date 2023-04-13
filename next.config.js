f79e562e5db235be63df69f96607f2aaeafa4957
applenicle <gamer31122002@gmail.com>
2023-04-13 19:16:21 +0500

    new packages & config language

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales : [
+      'en',
+      'ar',
+      'de',
+      'es',
+      'es-419',
+      'fr',
+      'it',
+      'ja',
+      'ko',
+      'pl',
+      'pt-BR',
+      'ru',
+      'tr',
+      'zh-CN',
+      'zh-Hant',
+    ],
+    defaultLocale: 'en',
+    localeDetection: true,
+  }
 }
 
 module.exports = nextConfig
