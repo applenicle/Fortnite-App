0e88db72c4536b2e711089a98523264db5863ee0
applenicle <gamer31122002@gmail.com>
2023-04-30 23:31:37 +0500

    layout

---
+++ layout/index.tsx
@@ -0,0 +1,27 @@
+import styles from './Layout.module.scss';
+import { LayoutProps } from './Layout.props';
+import { Sidebar, Footer, Header } from '@/components';
+import { FunctionComponent } from 'react';
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <main className={styles.body} tabIndex={0} role="main">
+        {children}
+      </main>
+      <Footer className={styles.footer} />
+    </div>
+  );
+};
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
+};
