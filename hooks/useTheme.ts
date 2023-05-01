ba6ef592d86b4835bb7312a33a4690c3ce0a0c86
applenicle <gamer31122002@gmail.com>
2023-05-01 20:24:15 +0500

    useDebounce hook

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,15 +1,14 @@
 import React from 'react';
 
 export const useTheme = () => {
-  // const [theme, setTheme] = React.useState(localStorage.getItem('theme-color') || 'dark');
-  const [theme, setTheme] = React.useState(!true);
+  const [theme, setTheme] = React.useState(localStorage.getItem('theme-color') || 'dark');
   React.useLayoutEffect(() => {
-    if (theme === true) {
-      document.documentElement.setAttribute('data-theme', 'light');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    // localStorage.setItem('theme-color', theme);
+    // if (theme === true) {
+    document.documentElement.setAttribute('data-theme', 'light');
+    // } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    // }
+    localStorage.setItem('theme-color', theme);
   }, [theme]);
   return { theme, setTheme };
 };
