3e16e53d0d6bd8fe83cd0615791880af2981a3ab
applenicle <gamer31122002@gmail.com>
2023-05-20 14:50:56 +0500

    Menu rework

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,57 +1,59 @@
 import styles from './Menu.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import cn from 'classnames';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useAppDispatch, useAppSelector } from '@/hooks/redux';
-import { setFistIndex, setIsOpen } from '@/redux/slices/Menu';
 import { navigation } from './data';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import cn from 'classnames';
 
-const Menu = (): JSX.Element => {
+const SidebarItem = ({ item }): JSX.Element => {
   const { locale, pathname } = useRouter();
+  const [open, setOpen] = useState(false);
 
-  const dispatch = useAppDispatch();
-  const { firstIndex, isOpen } = useAppSelector((state) => state.MenuSlice);
-  const checkIndex = (index: number) => {
-    dispatch(setIsOpen(!isOpen));
-    dispatch(setFistIndex(index));
-  };
   return (
     <>
       <ul className={styles.menu}>
-        {navigation.map((menu, i) => (
-          <li key={i}>
-            <div
-              onClick={() => checkIndex(i)}
-              className={cn(styles.link, {
-                [styles.linkActive]: firstIndex == i && isOpen,
-              })}>
-              <FontAwesomeIcon icon={menu.icon} />
-              {menu.label}
-              <FontAwesomeIcon icon={faArrowRight} />
-            </div>
-            <ul
-              className={cn(styles.secondList, {
-                [styles.secondListActive]: firstIndex == i && isOpen,
-              })}>
-              {menu.second.map((secondMenu, i) => (
-                <li key={i}>
-                  <Link
-                    className={cn(styles.linkSecond, {
-                      [styles.linkSecondActive]: pathname === secondMenu.route,
-                    })}
-                    locale={locale}
-                    href={secondMenu.route}>
-                    {secondMenu.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+        <li
+          onClick={() => setOpen(!open)}
+          className={cn(styles.item, {
+            [styles.itemActive]: open,
+          })}>
+          <div className={styles.title}>
+            <FontAwesomeIcon icon={item.icon} />
+            <h3>{item.label}</h3>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </div>
+          <ul
+            className={cn(styles.content, {
+              [styles.contentActive]: open,
+            })}>
+            {item.second.map((secondMenu, i: number) => (
+              <li key={i}>
+                <Link
+                  className={cn(styles.linkSecond, {
+                    [styles.linkSecondActive]: pathname === secondMenu.route,
+                  })}
+                  locale={locale}
+                  href={secondMenu.route}>
+                  {secondMenu.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </li>
       </ul>
     </>
+  );
+};
+
+const Menu = () => {
+  return (
+    <div>
+      {navigation.map((item, i: number) => (
+        <SidebarItem key={i} item={item} />
+      ))}
+    </div>
   );
 };
 
